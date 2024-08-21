047d61100b8d45bc0870acbafec615b9ca327e68
Tobias <st157488@stud.uni-stuttgart.de>
2024-08-21 16:54:06 +0200

    Add UDP game server info ping

--- H2MLauncher.Core/ViewModels/ServerBrowserViewModel.cs
+++ H2MLauncher.Core/ViewModels/ServerBrowserViewModel.cs
@@ -1,47 +1,72 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
 using H2MLauncher.Core.Models;
 using H2MLauncher.Core.Services;
+
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace H2MLauncher.Core.ViewModels
 {
     public class ServerBrowserViewModel : ObservableObject
     {
         private readonly RaidMaxService _raidMaxService;
-        private int totalServers = 0;
-        private int totalPlayers = 0;
+        private readonly ServerPingService _serverPingService;
+        private int _totalServers = 0;
+        private int _totalPlayers = 0;
 
         public IAsyncRelayCommand RefreshServersCommand { get; }
         public ObservableCollection<RaidMaxServer> Servers { get; private set; } = [];
 
         public int TotalServers
         {
-            get => totalServers;
-            private set => SetProperty(ref totalServers, value);
+            get => _totalServers;
+            private set => SetProperty(ref _totalServers, value);
         }
         public int TotalPlayers
         {
-            get => totalPlayers;
-            private set => SetProperty(ref totalPlayers, value);
+            get => _totalPlayers;
+            private set => SetProperty(ref _totalPlayers, value);
         }
 
-        public ServerBrowserViewModel(RaidMaxService raidMaxService)
+        public ServerBrowserViewModel(RaidMaxService raidMaxService, ServerPingService serverPingService)
         {
             _raidMaxService = raidMaxService ?? throw new ArgumentNullException(nameof(raidMaxService));
 
             RefreshServersCommand = new AsyncRelayCommand(LoadServersAsync);
+            _serverPingService = serverPingService;
         }
+
+        private CancellationTokenSource _loadCancellation = new();
 
         private async Task LoadServersAsync()
         {
-            Servers = new ObservableCollection<RaidMaxServer>(await _raidMaxService.GetServerInfosAsync(CancellationToken.None));
-            TotalServers = Servers.Count;
-            TotalPlayers = Servers.Sum(server => server.ClientNum);
+            await _loadCancellation.CancelAsync();
+            _loadCancellation = new();
 
-            OnPropertyChanged(nameof(Servers));
-            OnPropertyChanged(nameof(TotalServers));
-            OnPropertyChanged(nameof(TotalPlayers));
+            try
+            {
+                Servers.Clear();
+
+                var servers = await _raidMaxService.GetServerInfosAsync(_loadCancellation.Token);
+
+                TotalServers = 0;
+                TotalPlayers = 0;
+
+                await _serverPingService.StartRetrievingGameServerInfo(servers, (server, gameServer) =>
+                {
+                    server.Ping = gameServer.Ping;
+                    Servers.Add(server);
+
+                    TotalPlayers += server.ClientNum;
+                    TotalServers++;
+                }, _loadCancellation.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                // canceled
+            }
         }
     }
 }
