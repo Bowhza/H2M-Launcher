184e8f3e72e84382a1b7b9f7c35cf28c5f361aaf
Tobias <st157488@stud.uni-stuttgart.de>
2024-08-22 23:47:10 +0200

    update assembly version

--- H2MLauncher.UI/AssemblyInfo.cs
+++ H2MLauncher.UI/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -8,3 +9,8 @@ using System.Windows;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyTitle("Better H2M-Launcher")]
