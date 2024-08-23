bcf8f996c432a00843fa76caa04cd20dfcd04756
Bowhza <bogtank2003@gmail.com>
2024-08-23 14:23:30 -0600

    Added ping color

---
+++ H2MLauncher.UI/PingColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace H2MLauncher.UI;
+
+public class PingColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        long ping = (long)value;
+        Brush pingColor;
+
+        if (ping < 50)
+            pingColor = Brushes.LawnGreen;
+        else if (ping < 80)
+            pingColor = Brushes.Orange;
+        else
+            pingColor = Brushes.Red;
+
+        return pingColor;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
