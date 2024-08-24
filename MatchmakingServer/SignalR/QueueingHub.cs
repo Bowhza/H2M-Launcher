85fc123da95ba0d409b2e770f11e36c094cedc03
Tobias <st157488@stud.uni-stuttgart.de>
2024-08-24 14:07:29 +0200

    Add ASP.NET + SignalR matchmaking server project

---
+++ MatchmakingServer/SignalR/QueueingHub.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace MatchmakingServer.SignalR
+{
+    public class QueueingHub : Hub<IClient>
+    {
+        private readonly ConcurrentDictionary<string, string> _connectedPlayers = [];
+
+        public async Task<bool> JoinQueue(string serverIp, int serverPort, string playerName)
+        {
+            if (!_connectedPlayers.TryAdd(Context.ConnectionId, playerName))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public interface IClient
+    {
+        
+    }
+}
