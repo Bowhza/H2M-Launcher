7fa35c25b4f2a1ce6a2ed4ed57fced25198e44b1
Tobias <st157488@stud.uni-stuttgart.de>
2024-09-04 01:54:19 +0200

    Fix always queueing

--- H2MLauncher.UI/ViewModels/ServerBrowserViewModel.cs
+++ H2MLauncher.UI/ViewModels/ServerBrowserViewModel.cs
@@ -726,7 +726,7 @@ public partial class ServerBrowserViewModel : ObservableObject, IDisposable
         ServerData? serverData = _serverData.FirstOrDefault(d => 
             d.Ip == serverViewModel.Ip && d.Port == serverViewModel.Port);
 
-        int assumedMaxClients = serverViewModel.MaxClientNum - serverData?.PrivilegedSlots ?? 0;
+        int assumedMaxClients = serverViewModel.MaxClientNum - (serverData?.PrivilegedSlots ?? 0);
         if (serverViewModel.ClientNum >= assumedMaxClients) //TODO: check if queueing enabled
         {
             // server is full (TODO: check again if refresh was long ago to avoid unnecessary server communication?)
