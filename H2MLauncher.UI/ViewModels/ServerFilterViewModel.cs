fbe74b64353af55833b4130c3b2241818ea1f577
Tobi <mail@tobias-bodamer.de>
2024-09-06 12:40:14 +0200

    Fix buggy exclude keywords (#98)

--- H2MLauncher.UI/ViewModels/ServerFilterViewModel.cs
+++ H2MLauncher.UI/ViewModels/ServerFilterViewModel.cs
@@ -162,17 +162,10 @@ namespace H2MLauncher.UI.ViewModels
 
             // exclude keywords
 
+            ExcludeFilters.Clear();
             foreach (var (keyword, isEnabled) in settings.ExcludeKeywords)
             {
-                SelectableItem<string>? existingItem = ExcludeFilters.FirstOrDefault(i => i.Model.Equals(keyword));
-                if (existingItem is null)
-                {
-                    AddNewExcludeKeyword(keyword);
-                }
-                else
-                {
-                    existingItem.IsSelected = isEnabled;
-                }
+                AddNewExcludeKeyword(keyword, isEnabled);
             }
         }
 
@@ -195,9 +188,14 @@ namespace H2MLauncher.UI.ViewModels
         [RelayCommand(CanExecute = nameof(CanAddNexExcludeKeyword))]
         public void AddNewExcludeKeyword(string keyword)
         {
-            ExcludeFilters.Add(new(keyword, onRemove: () => RemoveExcludeKeyword(keyword))
+            AddNewExcludeKeyword(keyword, true);
+        }
+
+        private void AddNewExcludeKeyword(string keyword, bool isSelected)
+        {
+            ExcludeFilters.Add(new(keyword.ToLower(), onRemove: () => RemoveExcludeKeyword(keyword))
             {
-                IsSelected = true,
+                IsSelected = isSelected,
                 Name = keyword,
             });
         }
@@ -209,13 +207,13 @@ namespace H2MLauncher.UI.ViewModels
                 return false;
             }
 
-            return !ExcludeFilters.Any(i => i.Model.Equals(keyword));
+            return !ExcludeFilters.Any(i => i.Model.Equals(keyword, StringComparison.OrdinalIgnoreCase));
         }
 
         [RelayCommand]
         public void RemoveExcludeKeyword(string keyword)
         {
-            SelectableItem<string>? item = ExcludeFilters.FirstOrDefault(i => i.Model.Equals(keyword));
+            SelectableItem<string>? item = ExcludeFilters.FirstOrDefault(i => i.Model.Equals(keyword, StringComparison.OrdinalIgnoreCase));
             if (item is not null)
             {
                 ExcludeFilters.Remove(item);
