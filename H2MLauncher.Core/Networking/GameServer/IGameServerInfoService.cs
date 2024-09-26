066b79134cf5b0e6c59f7f4f930d5892033c6994
Tobias <st157488@stud.uni-stuttgart.de>
2024-09-26 20:32:52 +0200

    Adjust TCP game server info fetching - Increase parallel requests - Fix timout being per-request on callback based method

--- H2MLauncher.Core/Networking/GameServer/IGameServerInfoService.cs
+++ H2MLauncher.Core/Networking/GameServer/IGameServerInfoService.cs
@@ -5,11 +5,23 @@ namespace H2MLauncher.Core.Services
 {
     public interface IGameServerInfoService<TServer> where TServer : IServerConnectionDetails
     {
-        IAsyncEnumerable<(TServer server, GameServerInfo? info)> GetAllInfoAsync(IEnumerable<TServer> servers, int requestTimeoutInMs = 10000, CancellationToken cancellationToken = default);
-        Task<IAsyncEnumerable<(TServer server, GameServerInfo? info)>> GetInfoAsync(IEnumerable<TServer> servers, bool sendSynchronously = false, int requestTimeoutInMs = 10000, CancellationToken cancellationToken = default);
-        Task GetInfoAsync(IEnumerable<TServer> servers, Action<ServerInfoEventArgs<TServer, GameServerInfo>> onInfoResponse, int timeoutInMs = 10000, CancellationToken cancellationToken = default);
         Task<GameServerInfo?> GetInfoAsync(TServer server, CancellationToken cancellationToken);
-        Task<IDisposable> SendGetInfoAsync(IEnumerable<TServer> servers, Action<ServerInfoEventArgs<TServer, GameServerInfo>> onInfoResponse, CancellationToken cancellationToken = default);
-        Task<Task> SendGetInfoAsync(IEnumerable<TServer> servers, Action<ServerInfoEventArgs<TServer, GameServerInfo>> onInfoResponse, int timeoutInMs = 10000, CancellationToken cancellationToken = default);
+
+        IAsyncEnumerable<(TServer server, GameServerInfo? info)> GetAllInfoAsync(
+            IEnumerable<TServer> servers, 
+            int requestTimeoutInMs = 10000, 
+            CancellationToken cancellationToken = default);
+
+        Task<IAsyncEnumerable<(TServer server, GameServerInfo? info)>> GetInfoAsync(
+            IEnumerable<TServer> servers, 
+            bool sendSynchronously = false, 
+            int requestTimeoutInMs = 10000, 
+            CancellationToken cancellationToken = default);
+
+        Task<Task> SendGetInfoAsync(
+            IEnumerable<TServer> servers, 
+            Action<ServerInfoEventArgs<TServer, GameServerInfo>> onInfoResponse, 
+            int timeoutInMs = 10000, 
+            CancellationToken cancellationToken = default);
     }
 }
