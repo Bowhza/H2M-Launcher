ff0dd7a5479436cae38c7ee4c98a8f0bda08ac04
Tobias <st157488@stud.uni-stuttgart.de>
2025-05-24 03:30:31 +0200

    Add Users and UserKeys tables and entities

--- MatchmakingServer/Database/DatabaseContext.cs
+++ MatchmakingServer/Database/DatabaseContext.cs
@@ -1,7 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MatchmakingServer.Database.Entities;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace MatchmakingServer.Database;
 
 public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
 {
+    public DbSet<UserDbo> Users => Set<UserDbo>();
+
+    public DbSet<UserKeyDbo> UserKeys => Set<UserKeyDbo>();
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserDbo>()
+            .HasMany<UserKeyDbo>(u => u.Keys)
+            .WithOne(k => k.User)
+            .HasForeignKey(k => k.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserKeyDbo>()
+            .HasIndex(k => k.PublicKeySPKI)
+            .IsUnique();
+    }
 }
---
+++ MatchmakingServer/Database/Migrations/20250524011859_Users.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchmakingServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserKeys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PublicKeySPKI = table.Column<string>(type: "text", nullable: false),
+                    PublicKeyHash = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUsedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RevokedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserKeys", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserKeys_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserKeys_PublicKeySPKI",
+                table: "UserKeys",
+                column: "PublicKeySPKI",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserKeys_UserId",
+                table: "UserKeys",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserKeys");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
