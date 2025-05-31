cb5933296c2d2bc76ed849744a1182d86502df1a
Tobias <st157488@stud.uni-stuttgart.de>
2025-05-31 13:28:32 +0200

    Disable button after clicking on add friend

---
+++ H2MLauncher.UI/ViewModels/UserSearchResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace H2MLauncher.UI.ViewModels;
+
+public sealed partial class UserSearchResultViewModel : ObservableObject
+{
+    public required string Id { get; init; }
+
+    public required string UserName { get; init; }
+
+    public string? PlayerName { get; init; }
+
+    [ObservableProperty]
+    private bool _hasRequested = false;
+}
