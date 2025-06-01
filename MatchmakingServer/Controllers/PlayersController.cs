a94d4293dcd416855b8e929bfc84be70280dae8d
Tobias <st157488@stud.uni-stuttgart.de>
2025-06-01 15:03:56 +0200

    Add players and users controllers

---
+++ MatchmakingServer/Controllers/PlayersController.cs
@@ -0,0 +1,51 @@
+﻿using MatchmakingServer.Authentication;
+using MatchmakingServer.Core.Social;
+using MatchmakingServer.SignalR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchmakingServer.Controllers;
+
+[ApiController]
+[Authorize(AuthenticationSchemes = ApiKeyDefaults.AuthenticationScheme)]
+[Route("[controller]")]
+public class PlayersController : ControllerBase
+{
+    private readonly PlayerStore _playerStore;
+
+    public PlayersController(PlayerStore playerStore)
+    {
+        _playerStore = playerStore;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PlayerDto>>> GetPlayers()
+    {
+        IList<Player> players = await _playerStore.GetAllPlayers();
+
+        return Ok(players.Select(CreatePlayerDto));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<PlayerDto>> GetPlayerByUserId(string id)
+    {
+        Player? player = await _playerStore.TryGet(id);
+        if (player is null)
+        {
+            return NotFound();
+        }
+        return Ok(CreatePlayerDto(player));
+    }
+
+    private static PlayerDto CreatePlayerDto(Player player)
+    {
+        return new PlayerDto(
+            player.Id,
+            player.UserName,
+            player.Name,
+            player.GameStatus,
+            player.Party?.Id
+        );
+    }
+}
---
+++ MatchmakingServer/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using MatchmakingServer.Authentication;
+using MatchmakingServer.Core.Social;
+using MatchmakingServer.Database;
+using MatchmakingServer.Database.Entities;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchmakingServer.Controllers;
+
+[ApiController]
+[Authorize(AuthenticationSchemes = ApiKeyDefaults.AuthenticationScheme)]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DatabaseContext _dbContext;
+
+    public UsersController(DatabaseContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PlayerDto>>> GetAllUsers()
+    {
+        IList<UserDto> users = await _dbContext.Users
+            .Select(u => CreateUserDto(u))
+            .ToListAsync();
+
+        return Ok(users);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<PlayerDto>> GetUserById(Guid id)
+    {
+        UserDto? user = await _dbContext.Users
+            .Select(u => CreateUserDto(u))
+            .FirstOrDefaultAsync(u => u.Id == id); 
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    private static UserDto CreateUserDto(UserDbo user)
+    {
+        return new UserDto(
+            user.Id,
+            user.Name,
+            user.LastPlayerName,
+            user.CreationDate
+        );
+    }
+}
