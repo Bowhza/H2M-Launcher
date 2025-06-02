10399876c294ba6f95da3f0ab840ba3edde3697e
Tobias <st157488@stud.uni-stuttgart.de>
2025-06-02 19:21:37 +0200

    Add customization options Allow blurring background

---
+++ H2MLauncher.UI/Converters/ScaleOffsetMultiConverter.cs
@@ -0,0 +1,68 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace H2MLauncher.UI.Converters
+{
+    public class ScaleOffsetMultiConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// Converts a collection of values (original size and absolute offset) to a scaling factor.
+        /// The order of values in the MultiBinding should be:
+        /// [0] -> originalSize (double)
+        /// [1] -> absoluteOffset (double)
+        /// </summary>
+        /// <param name="values">An array of objects that the source bindings produce.
+        /// The first value should be the original size, and the second should be the absolute offset.</param>
+        /// <param name="targetType">The type of the binding target property (should be double).</param>
+        /// <param name="parameter">An optional parameter to be used in the converter (not used in this case, set to null).</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A double representing the calculated scaling factor.</returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length < 2)
+            {
+                // Not enough values provided
+                return 1.0;
+            }
+
+            bool invert = Equals(parameter, true);
+
+            if (values[0] is double originalSize && values[1] is double absoluteOffset)
+            {
+                if (originalSize <= 0)
+                {
+                    // Handle cases where original size is zero or negative.
+                    return 1.0; // No scaling if original size is invalid.
+                }
+
+                // Calculate the total amount to crop
+                double totalCrop = 2 * absoluteOffset;
+
+                // Ensure we don't try to scale to a negative or zero size
+                double scaledSize = originalSize - totalCrop;
+
+                if (scaledSize <= 0)
+                {
+                    // If the offset is too large, the content would disappear or invert.
+                    // Return a very small positive scale to indicate it's almost gone, or 0.0.
+                    return 0.0001; // Or 0.0 to make it disappear
+                }
+
+                return invert 
+                    ? originalSize / scaledSize 
+                    : scaledSize / originalSize;
+            }
+
+            // Return a default scale of 1.0 if inputs are invalid.
+            return 1.0;
+        }
+
+        /// <summary>
+        /// Not implemented for one-way binding scenario.
+        /// </summary>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("ScaleOffsetMultiConverter can only be used for one-way binding.");
+        }
+    }
+}
---
+++ H2MLauncher.UI/Services/CustomizationManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using H2MLauncher.Core.Settings;
+using H2MLauncher.Core.Utilities;
+
+using Nogic.WritableOptions;
+
+namespace H2MLauncher.UI.Services;
+
+public partial class CustomizationManager : ObservableObject
+{
+    internal const string DefaultBackgroundImagePath = "pack://application:,,,/H2MLauncher.UI;component/Assets/Background.jpg";
+    internal const double DefaultBackgroundBlur = 0;
+
+    private readonly IWritableOptions<H2MLauncherSettings> _settings;
+
+    [ObservableProperty]
+    private ImageSource _backgroundImage = new BitmapImage(new Uri(DefaultBackgroundImagePath));
+
+    [ObservableProperty]
+    private bool _loadingError;
+
+    [ObservableProperty]
+    private double _backgroundBlur = 0;
+
+    public CustomizationManager(IWritableOptions<H2MLauncherSettings> settings)
+    {
+        _settings = settings;
+    }
+
+    /// <summary>
+    /// Load the initial background image from the settings.
+    /// </summary>
+    public void LoadInitialValues()
+    {
+        BackgroundBlur = _settings.Value.Customization?.BackgroundBlur ?? DefaultBackgroundBlur;
+
+        if (string.IsNullOrEmpty(_settings.Value.Customization?.BackgroundImagePath))
+        {
+            LoadDefaultImage();
+            return;
+        }
+
+        if (!TryLoadImage(_settings.Value.Customization!.BackgroundImagePath, out var image))
+        {
+            LoadingError = true;
+            LoadDefaultImage();
+        }
+        else
+        {
+            BackgroundImage = image;
+        }        
+    }
+
+    public bool LoadImage(string imageFileName)
+    {
+        if (TryLoadImage(imageFileName, out var image))
+        {
+            BackgroundImage = image;
+            LoadingError = false;
+
+            UpdateCustomizationSettings(settings => settings with
+            {
+                BackgroundImagePath = imageFileName
+            });
+
+            return true;
+        }
+        else
+        {
+            LoadingError = true;
+            return false;
+        }
+    }
+
+    public void LoadDefaultImage()
+    {
+        BackgroundImage = new BitmapImage(new Uri(DefaultBackgroundImagePath));
+        LoadingError = false;
+
+        UpdateCustomizationSettings(settings => settings with
+        {
+            BackgroundImagePath = null
+        });
+    }
+
+    private static bool TryLoadImage(string path, [NotNullWhen(true)] out ImageSource? image)
+    {
+        image = null;
+        try
+        {
+            if (!File.Exists(path))
+            {
+                return false;
+            }
+
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(path);
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.EndInit();
+            bitmap.Freeze();
+
+            image = bitmap;
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    partial void OnBackgroundBlurChanged(double value)
+    {
+        UpdateCustomizationSettings(settings => settings with
+        {
+            BackgroundBlur = value
+        });
+    }
+
+    private void UpdateCustomizationSettings(Func<LauncherCustomizationSettings, LauncherCustomizationSettings> update)
+    {
+        _settings.Update((settings) => settings with
+        {
+            Customization = update(settings.Customization ?? new())
+        });
+    }
+}
