ddff61e4a47fadc2e291b6df24bf5fc83d5850ef
Tobi <mail@tobias-bodamer.de>
2025-06-21 23:33:29 +0200

    Bump version to 2.5.5 beta (#218)
    * Bump version to 2.5.5 beta
    
    * Add launch profiles to ease switching server environments
    
    * Minor fixes
    - Move join button below server in popup
    - Fix color of party size icon
    - Make recent player server scroll in list
    - Display sanitized server name in recent player tooltip
    - Make friend requests non-toggleable
    
    * set recent players limit to 50

---
+++ H2MLauncher.UI/ViewModels/Social/PlayingServerViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using H2MLauncher.Core.Models;
+
+namespace H2MLauncher.UI.ViewModels
+{
+    public partial class PlayingServerViewModel : ObservableObject, ISimpleServerInfo
+    {
+        public required string Ip { get; init; }
+
+        public required int Port { get; init; }
+
+        public required string ServerName { get; init; }
+
+        [ObservableProperty]
+        private string _mapDisplayName = "";
+
+        [ObservableProperty]
+        private string _gameTypeDisplayName = "";
+
+        public required DateTimeOffset JoinedAt { get; init; }
+
+        [ObservableProperty]
+        private TimeSpan _playingTime = TimeSpan.Zero;
+
+        public string Status => this switch
+        {
+            { GameTypeDisplayName: not null, MapDisplayName: not null } =>
+                $"{GameTypeDisplayName} on {MapDisplayName}",
+            { MapDisplayName: not null } => $"Playing on {MapDisplayName}",
+            _ => ""
+        };
+
+        public string SanitizedServerName => ColorCodeSequenceRegex().Replace(ServerName, "");
+
+
+        public void RecalculatePlayingTime()
+        {
+            PlayingTime = DateTimeOffset.Now - JoinedAt;
+        }
+
+        [GeneratedRegex(@"(\^\d)")]
+        private static partial Regex ColorCodeSequenceRegex();
+    }
+}
